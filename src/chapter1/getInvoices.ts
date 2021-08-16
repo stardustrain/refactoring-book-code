4b39b414afb7d3ff37e51abb53002985fa9c5a65
stardustrain <kthanterran@gmail.com>
2021-08-16 15:59:03 +0900

    refactor: add renderPlainText from statement function

--- src/chapter1/getInvoices.ts
+++ src/chapter1/getInvoices.ts
@@ -10,19 +10,32 @@ import invoices from './invoices.json';
 
 import type {Invoice} from './type';
 
-const statement = (invoice: Invoice) => {
-  let result = `Result (Customer: ${invoice.customer})\n`;
+type StatementData = {
+  customer: Invoice['customer'];
+  performances: Invoice['performances'];
+};
+const renderPlainText = (statementData: StatementData) => {
+  const {customer, performances} = statementData;
 
-  for (const perf of invoice.performances) {
+  let result = `Result (Customer: ${customer})\n`;
+  for (const perf of performances) {
     result += `${playFor(perf).name}: ${usd(getAmount(perf))} (${
       perf.audience
     }석)\n`;
   }
 
-  result += `total: ${usd(getTotalAmount(invoice.performances))}\n`;
-  result += `적립 포인트: ${getVolumeCredits(invoice.performances)}점\n`;
+  result += `total: ${usd(getTotalAmount(performances))}\n`;
+  result += `적립 포인트: ${getVolumeCredits(performances)}점\n`;
 
   return result;
+};
+
+const statement = (invoice: Invoice) => {
+  const statementData = {
+    customer: invoice.customer,
+    performances: {...invoice.performances},
+  };
+  return renderPlainText(statementData);
 };
 
 console.info(statement(invoices[0]));
